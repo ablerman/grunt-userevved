860209c9359c2d4b517094c5c60d89d5576bf3e1
Sandy Lerman <sandy@presencelearning.com>
2014-10-13 14:17:30 -0700

    Fix the map/js files to refer to each other

--- tasks/userevved.js
+++ tasks/userevved.js
@@ -1,6 +1,6 @@
 /*
  * grunt-userevved
- * 
+ *
  *
  * Copyright (c) 2014 Sandy Lerman
  * Licensed under the MIT license.
@@ -8,44 +8,73 @@
 
 'use strict';
 
+var _ = require('lodash');
+var json = require('json3');
+
 module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('userevved', 'Use revved filenames for static assets.', function () {
+    grunt.registerMultiTask('userevved', 'Use revved filenames for static assets.', function () {
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+        var summary = grunt.filerev.summary;
+        console.log(summary);
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+
+        this.processMap = function() {
+            var mapfiles = grunt.file.expand(this.data.map);
+            _.each(mapfiles, function(path) {
+                var pathParts = path.split('/');
+                var justThePath = pathParts.slice(0, pathParts.length-1).join('/');
+                var content = grunt.file.readJSON(path);
+
+                var lookupVal = justThePath + '/' + content.file;
+                var newFile = summary[lookupVal];
+                var parts = newFile.split('/');
+
+                content.file = parts[parts.length-1]
+                grunt.file.write(path, json.stringify(content) );
+            });
+        };
+        this.processMap();
+
+        this.processJs = function() {
+            var jsFiles = grunt.file.expand(this.data.js);
+            _.each(jsFiles, function(path) {
+                var pathParts = path.split('/');
+                var justThePath = pathParts.slice(0, pathParts.length-1).join('/');
+                var content = grunt.file.read(path);
+
+                var re = /\/\/# sourceMappingURL=(.+)/gm
+                var matches = content.match(re);
+                content = content.replace(re, function(match, group) {
+                    var lookupVal = justThePath + '/' + group;
+                    var newFile = summary[lookupVal];
+                    var parts = newFile.split('/');
+                    return '//# sourceMappingURL=' + parts[parts.length-1];
+                });
+                grunt.file.write(path, content);
+            });
+        };
+        this.processJs();
+
+
+        var processHtml = function() {
+
+        };
+
+        var processCss = function() {
+
+        };
+
+        var processJs = function() {
+
+        };
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
-  });
-
 };
